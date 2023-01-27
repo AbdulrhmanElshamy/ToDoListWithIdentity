decc02650ac2b684502841b15e632cf2e8d36920
AbDulrhman Elshamy <AbdulrhmanElshamy12@gmail.com>
2023-01-28 00:28:24 +0200

    edit

--- ToDoListWithIdentity/Cores/Models/ToDo.cs
+++ ToDoListWithIdentity/Cores/Models/ToDo.cs
@@ -6,17 +6,17 @@ namespace ToDoListWithIdentity.Cores.Models
     {
         public int Id { get; set; }
 
-        [MaxLength(150)]
+        [MaxLength(150),MinLength(5,ErrorMessage ="min lenght is 5 letter")]
         public string Title { get; set; } = null!;
 
         public string Description { get; set; } = null!;
 
         public DateTime CreatedOn { get; set; } = DateTime.Now;
 
-        public bool InProgress { get; set; }
+        public bool InProgress { get; set; } = true;
 
         public bool IsCompleted { get; set; }
 
-        public virtual string ApplicationUserId { get; set; } = null!;
+        public  string? ApplicationUserId { get; set; } = null!;
     }
 }
